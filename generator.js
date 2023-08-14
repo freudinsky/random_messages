07a5b7c04445073a9bc231c381747427b45d795d
freudinsky <136381953+freudinsky@users.noreply.github.com>
2023-08-14 18:58:36 +0200

    ..

---
+++ generator.js
@@ -0,0 +1,23 @@
+import { msgCollection } from "./msg-parse.js";
+
+const btn = document.querySelector(".btn");
+const outputBg = document.querySelector(".output");
+const output = document.querySelector(".output>p");
+
+function randomMsg(nr) {
+	return msgCollection[nr];
+}
+function writeMsg() {
+    let random = Math.floor(Math.random() * 100);
+	outputBg.removeAttribute("class", "output");
+	outputBg.setAttribute("class", "output-on");
+    output.append(`"${randomMsg(random)}"`);
+}
+
+function btnClick() {
+    output.textContent = ""   
+    writeMsg()
+
+}
+
+btn.addEventListener("click", () => btnClick());
